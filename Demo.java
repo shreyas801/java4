0e111d800a4a87d53fca482088dd3678dc8f6b7a
shreyas801 <shreyastotagi142@gmail.com>
2025-04-23 14:45:32 +0530

    java file

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=5;
+        int sum = a+b;
+        int sub = a-b;
+        System.out.println("Add is: "+sum);
+        System.out.println("Sub is: "+sub);
+    }
+}
